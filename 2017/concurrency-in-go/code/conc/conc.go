3583117eb71d364e28e138441cb001ff2395495b
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-02-09 22:32:26 +0100

    Concurrency in Go: better example

--- 2017/concurrency-in-go/code/conc/conc.go
+++ 2017/concurrency-in-go/code/conc/conc.go
@@ -1,20 +1,20 @@
 package main
 
 func main() {
-    // (1) create a channel
-    c := make(chan int32)
+	// (1) create a channel
+	c := make(chan int32)
 
-    // (2) anonymous function is invoked in the goroutine
-    go func(ch chan int32) {
+	// (2) anonymous function is invoked in the goroutine
+	go func(ch chan int32) {
 
-        // (3) wait for the data from the channel
-        value := <-ch
+		// (3) wait for the data from the channel
+		value := <-ch
 
-        if value == 42 {
-            println("\\ʕ◔ϖ◔ʔ/")
-        }
-    }(c)
+		if value == 42 {
+			println("\\ʕ◔ϖ◔ʔ/")
+		}
+	}(c)
 
-    // (4) send value to the channel
-    c <- 42
+	// (4) send value to the channel
+	c <- 42
 }
---
+++ 2017/concurrency-in-go/code/worker/worker.go
@@ -0,0 +1,109 @@
+package main
+
+import "fmt"
+import "time"
+import "sync"
+import "sync/atomic"
+
+// START_POOL OMIT
+type WorkerPool struct {
+    wg sync.WaitGroup
+    sz int32
+    ch <-chan int
+    fn func(int)
+}
+
+// END_POOL OMIT
+
+// START_NEW OMIT
+func New(amount int32, jobs <-chan int, f func(int)) *WorkerPool {
+    return &WorkerPool{
+        wg: sync.WaitGroup{},
+        sz: amount,
+        ch: jobs,
+        fn: f,
+    }
+}
+
+// END_NEW OMIT
+
+// START_START OMIT
+func (w *WorkerPool) Start() {
+    sz, w.sz := w.sz, 0
+    for i := int32(1); i <= sz; i++ {
+        w.invoke(i)
+    }
+}
+
+// END_START OMIT
+
+// START_INVOKE OMIT
+func (w *WorkerPool) invoke(id int32) {
+    w.wg.Add(1)
+    atomic.AddInt32(&w.sz, 1)
+    go func() {
+        for j := range w.ch {
+            fmt.Println("worker", id, "started  job", j)
+            w.fn(500)
+            fmt.Println("worker", id, "finished job", j)
+
+            if atomic.LoadInt32(&w.sz) < id {
+                w.wg.Done()
+                return
+            }
+        }
+        w.wg.Done()
+    }()
+}
+
+// END_INVOKE OMIT
+
+// START_INC OMIT
+func (w *WorkerPool) Inc(delta int32) {
+    sz := atomic.LoadInt32(&w.sz)
+    for i := int32(1); i < delta; i++ {
+        w.invoke(i + sz)
+    }
+}
+
+// END_INC OMIT
+
+// START_DEC OMIT
+func (w *WorkerPool) Dec(delta int32) {
+    atomic.AddInt32(&w.sz, -delta)
+}
+
+// END_DEC OMIT
+
+// START_WAIT OMIT
+func (w *WorkerPool) Wait() {
+    w.wg.Wait()
+}
+
+// END_WAIT OMIT
+
+func main() {
+    jobs := make(chan int)
+    funn := func(i int) { time.Sleep(time.Duration(i) * time.Millisecond) }
+    pool := New(3, jobs, funn)
+
+    pool.Start()
+
+    go func() {
+        funn(1700)
+        println("oh no...")
+        pool.Dec(2)
+    }()
+    go func() {
+        funn(10700)
+        println("OH MYYYYY!!! \\ʕ◔ϖ◔ʔ/")
+        pool.Inc(8)
+    }()
+
+    for i := 1; i <= 100; i++ {
+        jobs <- i
+    }
+    close(jobs)
+
+    pool.Wait()
+}
